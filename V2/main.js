9913bf877cd32d00738071050ba56053c767abff
BxsedSosa <donovanxabad@gmail.com>
2024-01-01 01:15:36 -0800

    Completed v2 of site

--- V2/main.js
+++ V2/main.js
@@ -1,11 +1,24 @@
+// todos before (before the code is ran i believe that todo still initialize then put in the array of elements that are being called for 100%) (undefined)
 var todos = ['Item 1','Item 2','Item 3'];
-console.log(todos);
+// todos after (todos would have the 3 elements placed into the array 100%) (0: "Item 1"1: "Item 2"2: "Item 3")
 
-todos.push('Item 4');
 console.log(todos);
+//(['Item 1','Item 2','Item 3'] 100%) (['Item 1', 'Item 2', 'Item 3'])
 
-todos[0] = 'Item 1 updated';
-console.log(todos);
+//function to add new text into array
+function addText(newText) {
+  todos.push(newText);
+  console.log(todos);
+}
 
-todos.splice(0, 1);
-console.log(todos)
+//function lets you edit text
+function editText(index, changeText){
+  todos[index] = changeText;
+  console.log(todos);
+}
+
+//function lets you delete elements from array
+function delText(index){
+  todos.splice(index, 1);
+  console.log(todos);
+}
